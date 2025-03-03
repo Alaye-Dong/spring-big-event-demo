7a314fec0c7188b5b4353768dcfb5b9af5e6a1dc
Alaye-Dong <AlayeVast@outlook.com>
2025-03-03 15:38:06 +0800

    feat: 04_注册接口参数校验

--- src/main/java/org/example/bigevent/controller/UserController.java
+++ src/main/java/org/example/bigevent/controller/UserController.java
@@ -1,22 +1,26 @@
 package org.example.bigevent.controller;
 
+import jakarta.validation.constraints.Pattern;
 import org.example.bigevent.pojo.Result;
 import org.example.bigevent.pojo.User;
 import org.example.bigevent.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username, String password) {
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
+
         User user = userService.findByUserName(username);
         if (user == null) {
             userService.register(username, password);
@@ -24,5 +28,6 @@ public class UserController {
         } else {
             return Result.error("用户名已存在");
         }
+
     }
 }
