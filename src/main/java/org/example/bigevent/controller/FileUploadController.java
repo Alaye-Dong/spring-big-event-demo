2784b3518a0c400289e23935a935f8ee15065d72
Alaye-Dong <AlayeVast@outlook.com>
2025-03-09 18:28:48 +0800

    feat: 26_文件上传

---
+++ src/main/java/org/example/bigevent/controller/FileUploadController.java
@@ -0,0 +1,22 @@
+package org.example.bigevent.controller;
+
+import org.example.bigevent.pojo.Result;
+import org.example.bigevent.utils.AliOssUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+public class FileUploadController {
+
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) throws Exception {
+        String originalFilename = file.getOriginalFilename();
+        String filename = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
+        String url = AliOssUtil.uploadFile(filename, file.getInputStream());
+        return Result.success(url);
+    }
+}
